427fb2ac3ae0a10cce3d90ef73794556b593cd5c
Wojtek Suryn <wojciech.suryn@gmail.com>
2015-11-08 14:13:55 +0100

    post enabled

--- src/LoginView.java
+++ src/LoginView.java
@@ -1,16 +1,16 @@
 
-import org.apache.commons.httpclient.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Objects;
 
-/**
- * Created by kamilwysocki on 08/11/15.
- */
 public class LoginView extends JFrame {
     private JButton loginButton;
     private JTextField loginTextField;
@@ -29,20 +29,24 @@ public class LoginView extends JFrame {
                 if (Objects.equals(loginTextField.getText(), "admin")) {
 
 
-//
-//                    HttpClient client = new DefaultHttpClient();
-//                    HttpPost post = new HttpPost("http://localhost:9998/userservice/create");
-//                    StringEntity input = new StringEntity("{\"login\":\"Jan Kowalski\",\"password\":\"123\"}");
-//                    input.setContentType("application/json");
-//                    post.setEntity(input);
-//                    HttpResponse response = client.execute(post);
-//                    BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-//                    String line = "";
+                    try {
+                        DefaultHttpClient client = new DefaultHttpClient();
+                        HttpPost post = new HttpPost("http://localhost:9998/userservice/create");
+                        StringEntity input = new StringEntity("{\"login\":\"Jan Kowalski\",\"password\":\"123\"}");
+
+                        input.setContentType("application/json");
+                        post.setEntity(input);
+                        org.apache.http.HttpResponse response = client.execute(post);
+                        BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                        String line = "";
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
 
                     MainView mainView = new MainView();
                     mainView.setVisible(true);
                 } else {
-                    JOptionPane.showMessageDialog(LoginView.this,"Złe hasło lub login");
+                    JOptionPane.showMessageDialog(LoginView.this, "Złe hasło lub login");
                 }
             }
         });
